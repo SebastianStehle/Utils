1e5fe7f22fdbc840a8713a4f9064805ca7212a9b
Sebastian <sebastian.stehle@busliniensuche.de>
2016-03-08 17:25:16 +0100

    Desktop detector and behavior

---
+++ GP.Utils.Uwp/PlattformDetector.cs
@@ -0,0 +1,30 @@
+﻿// ==========================================================================
+// PlattformDetector.cs
+// Hercules Mindmap App
+// ==========================================================================
+// Copyright (c) Sebastian Stehle
+// All rights reserved.
+// ==========================================================================
+
+using System;
+using Windows.System.Profile;
+
+namespace GP.Utils
+{
+    /// <summary>
+    /// Detects the current plattform.
+    /// </summary>
+    public static class PlattformDetector
+    {
+        /// <summary>
+        /// Determines whether the current plattform is a desktop device.
+        /// </summary>
+        public static bool IsDesktop
+        {
+            get
+            {
+                return string.Equals(AnalyticsInfo.VersionInfo.DeviceFamily, "Windows.Desktop", StringComparison.OrdinalIgnoreCase);
+            }
+        }
+    }
+}
---
+++ GP.Utils.Uwp/UI/Interactivity/ShowOnDesktopBehavior.cs
@@ -0,0 +1,30 @@
+﻿// ==========================================================================
+// ShowOnDesktopBehavior.cs
+// Hercules Mindmap App
+// ==========================================================================
+// Copyright (c) Sebastian Stehle
+// All rights reserved.
+// ==========================================================================
+
+using Windows.UI.Xaml;
+
+namespace GP.Utils.UI.Interactivity
+{
+    /// <summary>
+    /// Shows the associated element when running on desktop device.
+    /// </summary>
+    public sealed class ShowOnDesktopBehavior : Behavior<UIElement>
+    {
+        /// <summary>
+        /// Called after the behavior is attached to an AssociatedObject.
+        /// </summary>
+        /// <remarks>Override this to hook up functionality to the AssociatedObject.</remarks>
+        protected override void OnAttached()
+        {
+            if (PlattformDetector.IsDesktop)
+            {
+                AssociatedElement.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
